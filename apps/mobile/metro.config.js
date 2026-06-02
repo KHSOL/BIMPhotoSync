4823ecd49903ecd4e5d78eb8d82212aceef1c20d
Hansol Kim <khsol3141@gmail.com>
2026-06-02 17:42:20 +0900

    Fix Expo Go mobile bundling

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const { getDefaultConfig } = require("expo/metro-config");
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, "../..");
+
+const config = getDefaultConfig(projectRoot);
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules")
+];
+config.resolver.extraNodeModules = {
+  react: path.resolve(projectRoot, "node_modules/react")
+};
+
+module.exports = config;
